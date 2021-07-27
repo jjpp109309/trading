a27c8a83daf685b0324b9e3ca1e0ec8a60bc8d91
Jaime Paredes <jjpp109309@gmail.com>
2021-07-27 00:44:05 -0500

    feat: finished

--- src/bestTradingDay.py
+++ src/bestTradingDay.py
@@ -3,6 +3,9 @@ __all__ = ['getBestTradingDay']
 # libraries
 import os
 import requests
+import datetime
+import pdb
+
 import numpy as np
 import pandas as pd
 from dotenv import load_dotenv
@@ -22,7 +25,7 @@ def getBestTradingDay(ticker, dateFrom, dateTo, apiKey='polygonKey'):
     apiKey = os.environ[apiKey]
 
     # initialize input params
-    baseUrl = 'https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from}/{to}?adjusted=true&sort=asc&limit=120&apiKey={apiKey}'
+    baseUrl = 'https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from}/{to}?adjusted=true&sort=asc&apiKey={apiKey}'
     params = {
         'stocksTicker': ticker,
         'multiplier': 1,
@@ -49,10 +52,29 @@ def getBestTradingDay(ticker, dateFrom, dateTo, apiKey='polygonKey'):
     groupCols = ['ticker', 'year', 'month']
     df['tradingDayOfMonth'] = df.groupby(groupCols)['date'].rank(method='dense')
 
-    print(df.head())
+    # best trading day
+    df['amountMonthRank'] = df.groupby(groupCols)['l'].rank(method='dense')
+    df['isLowestOfMonth'] = (df['amountMonthRank'] == 1) * 1
 
-def main():
-    getBestTradingDay('TSLA', '2021-01-01', '2021-06-01')
+    dfBest = df.groupby('tradingDayOfMonth', as_index=False)\
+        .agg({'isLowestOfMonth': 'sum'})\
+        .sort_values('isLowestOfMonth', ascending=False)
+
+
+    return dfBest
+
+
 
 if __name__ == '__main__':
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ticker')
+    args = parser.parse_args()
+    print(args)
+    df = getBestTradingDay(args.ticker, '2019-07-01', '2021-07-01')
+
+    print(df.head())
+
+
+
